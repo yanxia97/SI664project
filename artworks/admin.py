da8de84e2d1db8758cbf518767adfa67f211e7af
yanxia97 <isaacyan@umich.edu>
2018-12-18 16:49:51 -0500

    Update admin.py

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,3 +1,80 @@
 from django.contrib import admin
 
-# Register your models here.
+import artworks.models as models
+
+@admin.register(models.Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    fields = [
+        'artist_name',
+        'gender',
+        'birth_place',
+        'death_place',
+        'birth_year',
+        'death_year'
+    ]
+
+    list_display = [
+        'artist_name',
+        'gender',
+        'birth_place',
+        'death_place',
+        'birth_year',
+        'death_year'
+    ]
+
+    list_filter = ['gender', 'birth_year', 'death_year']
+
+@admin.register(models.ArtistRole)
+class ArtistRoleAdmin(admin.ModelAdmin):
+    fields = ['artist_role_name']
+    list_display = ['artist_role_name']
+    ordering = ['artist_role_name']
+
+@admin.register(models.Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    fields = ['original_id', 'subject_name', 'parent_subject']
+    list_display = ['original_id', 'subject_name', 'parent_subject']
+    ordering = ['subject_name']
+
+@admin.register(models.Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    fields = [
+        'artwork_name',
+        'accession_number',
+        'artist',
+        'artist_role',
+        'date_text',
+        'medium',
+        'credit_line',
+        'acquisition_year',
+        (
+            'width',
+            'height',
+            'depth'
+        )        
+    ]
+
+    list_display = [
+        'artwork_name',
+        'accession_number',
+        'artist',
+        'artist_role',
+        'date_text',
+        'medium',
+        'acquisition_year',
+        'subject_display'
+    ]
+
+    list_filter = ['artist_role', 'acquisition_year']
+
+@admin.register(models.Gender)
+class GenderAdmin(admin.ModelAdmin):
+    fields = ['gender_name']
+    list_display = ['gender_name']
+    ordering = ['gender_name']
+
+@admin.register(models.Place)
+class PlaceAdmin(admin.ModelAdmin):
+    fields = ['place_name']
+    list_display = ['place_name']
+    ordering = ['place_name']
